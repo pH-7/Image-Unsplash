7d5cef58617c4ab21d14e8d5ea46e9168d7da8f8
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-08-27 19:35:11 +0100

    Add exception class

---
+++ src/Image/InvalidFitException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        MIT License <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+declare(strict_types = 1);
+
+namespace PH7\Image;
+
+use InvalidArgumentException;
+
+class InvalidFitException extends InvalidArgumentException
+{
+
+}
