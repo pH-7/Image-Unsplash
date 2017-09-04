e8924180eba9e6b5b2dc84930f03fd8d1fb916f4
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-09-04 05:17:26 +0100

    Add new Invalid Auto Exception

---
+++ src/Image/InvalidAutoException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        MIT License <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+namespace PH7\Image;
+
+use InvalidArgumentException;
+
+class InvalidAutoException extends InvalidArgumentException
+{
+
+}
